1a9877c9095f7bb87fdbc331ba0987f011bb1f5a
mijelilucero <mlucerob@miumg.edu.gt>
2026-09-04 17:04:13 -0600

    feat: agrega dashboard inicial y conecta clientes al layout con header/footer

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,12 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Clientes extends CI_Controller
+class Dashboard extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Cliente_model');
     }
 
     public function index()
